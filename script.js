f26d9329e59471aeeb39772d1a107bde9edf16cd
Harry Steele <harrysteele@harrys-air.lan>
2022-09-13 16:29:45 -0400

    empty the form if full added

--- script.js
+++ script.js
@@ -9,19 +9,27 @@ const inputs = document.querySelectorAll("input");
 form.addEventListener("submit", function(e) {
     e.preventDefault();
 
+    const inputValues = [];
+
     for(let input of inputs) {
-        console.log(input.value);
 
         // if input is empty change background color to red and show error message
         if(input.value === ""){
             input.style.borderColor = "red";
-            console.log(input.nextElementSibling);
             input.nextElementSibling.classList.remove("hidden");
 
             // if input is filled remove error message and change color back to black
         } else {
             input.nextElementSibling.classList.add("hidden");
             input.style.borderColor = "black";
+            inputValues.push(input.value);
+        }
+
+        // if all inputs are filled empty the form
+        if(inputValues.length === 4) {
+            for(let input of inputs) {
+                input.value = "";
+            }
         }
     }
 });
